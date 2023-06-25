bbe22f4830d55cb74ec8032863ce613b794b9f0f
CamaehoRilynn1994 <137526477+CamaehoRilynn1994@users.noreply.github.com>
2023-06-25 12:19:17 +0700

    Enhance search result relevancy

---
+++ garden_island_house.swift
@@ -0,0 +1,54 @@
+// MARK: - Public Speaking Workshop
+
+import Foundation
+
+// MARK: - Workshop Introduction
+func welcome() {
+    print("Welcome to the Public Speaking Workshop! We'll be discussing the basics of public speaking and how to become a more successful and engaging speaker. Let's get started!"
+}
+
+// MARK: - Workshop Breakdown
+func workshopBreakdown() {
+    print("This workshop will be broken into three sections:")
+    print("1. Understanding the basics of public speaking")
+    print("2. How to prepare for a public speaking event")
+    print("3. Tips for delivering a successful presentation")
+}
+
+// MARK: - Basics of Public Speaking
+func basics() {
+    print("The basics of public speaking involve creating an introduction, using effective body language, and creating a powerful closing. Each of these aspects have a significant impact on the success of your presentation.")
+}
+
+// MARK: - Prepare for a Public Speaking Event
+func prepare() {
+    print("To prepare for a public speaking event, you should practice your presentation in advance. It's also important to know your audience and create a presentation geared towards them. Lastly, be sure to practice delivering your presentation out loud.")
+}
+
+// MARK: - Deliver a Successful Presentation
+func deliver() {
+    print("When delivering a successful presentation, there are many things to consider. First, be sure to maintain strong eye contact with your audience. Secondly, don’t be afraid to pause for dramatic effect. Lastly, be sure to finish strongly and make sure you address all of your main points.")
+}
+
+// MARK: - Summary
+func summary() {
+    print("We have covered the basics of public speaking, how to prepare for a public speaking event, and tips for delivering a successful presentation. If you follow these guidelines, you will be able to become a more successful and engaging speaker.")
+}
+
+// MARK: - Workshop Conclusion
+func conclusion()  {
+    print("That concludes the Public Speaking Workshop. Thank you for attending and I hope these tips have been helpful!")
+}
+
+// MARK: - Main
+func main() {
+    welcome()
+    workshopBreakdown()
+    basics()
+    prepare()
+    deliver()
+    summary()
+    conclusion()
+}
+
+main()
